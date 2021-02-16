1b5d36fb6345be9ff43537ad5073d4d7fc1b133d
Perpetua-Kabute <kabuteperpetua@gmail.com>
2021-02-16 03:20:15 +0300

    feat: Sign Up User

--- app/src/main/java/com/example/elokira/SignUpFragment.kt
+++ app/src/main/java/com/example/elokira/SignUpFragment.kt
@@ -1,13 +1,18 @@
 package com.example.elokira
 
+import User
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.elokira.databinding.SignUpFragmentBinding
+import com.zhuinden.liveevent.observe
 
 class SignUpFragment : Fragment() {
 
@@ -29,7 +34,52 @@ class SignUpFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
+        val emailErr = binding.emailErr
+        val passwordErr = binding.passwordErr
+
+
         // TODO: Use the ViewModel
+        binding.signUp.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val user: User = User(email, password)
+            viewModel.signUpUser(user)
+            Log.i("Sign Up Fragment", "User is ${user}")
+        }
+
+        viewModel.loginResult.observe(this) { result ->
+            when (result) {
+                SignUpViewModel.LoginResult.EmailMissing -> {
+                    Toast.makeText(
+                        context, "Data is missing", Toast.LENGTH_LONG
+                    ).show()
+                    emailErr.error = "Email required"
+                    emailErr.requestFocus()
+                }
+                SignUpViewModel.LoginResult.PasswordMissing -> {
+                    passwordErr.error = "Password required"
+                    passwordErr.requestFocus()
+                }
+                SignUpViewModel.LoginResult.NetworkFailure -> {
+                }
+//                SignUpViewModel.LoginResult.NetworkError -> {
+//                    showToast(context, result.userMessage)
+//                }
+//                SignUpViewModel.LoginResult.Success -> {
+//                    val intent = Intent(applicationContext, HomeActivity::class.java)
+//                    intent.flags =
+//                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                    showToast(applicationContext, res.body()?.message)
+//                    Log.d("kjsfgxhufb", response.body()?.status.toString())
+//                    startActivity(intent)
+//                    finish()
+//                }
+                else ->Toast.makeText(context, "Network Error", Toast.LENGTH_LONG)
+            }
+
+        }
+
     }
+
 
 }
--- app/src/main/java/com/example/elokira/SignUpViewModel.kt
+++ app/src/main/java/com/example/elokira/SignUpViewModel.kt
@@ -1,7 +1,54 @@
 package com.example.elokira
 
+import User
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.zhuinden.eventemitter.EventEmitter
+import com.zhuinden.eventemitter.EventSource
 
 class SignUpViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+    sealed class LoginResult {
+        object EmailMissing : LoginResult()
+
+        object PasswordMissing : LoginResult()
+
+        class NetworkError(val userMessage: String) : LoginResult()
+
+        object NetworkFailure : LoginResult()
+
+        object Success : LoginResult()
+    }
+
+
+    private val loginResultEmitter = EventEmitter<LoginResult>()
+    val loginResult: EventSource<LoginResult> = loginResultEmitter
+
+
+
+    init{
+        Log.i("sign up ViewModel", " View model created")
+    }
+
+    fun signUpUser(userEntry: User){
+        //check if fields are empty
+        if(userEntry.email.isEmpty()){
+            loginResultEmitter.emit(LoginResult.EmailMissing)
+        }
+        if(userEntry.password.isEmpty()){
+            loginResultEmitter.emit(LoginResult.PasswordMissing)
+        }
+    }
+
+    fun addUser(){
+        //add user to database via an API
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+
+
 }
