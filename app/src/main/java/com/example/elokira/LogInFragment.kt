845a909718cac583ef3f33be8751962bfaddd44f
Perpetua-Kabute <kabuteperpetua@gmail.com>
2021-02-19 13:46:15 +0300

    fix: Correct navigation

--- app/src/main/java/com/example/elokira/LogInFragment.kt
+++ app/src/main/java/com/example/elokira/LogInFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.elokira.databinding.LogInFragmentBinding
 
@@ -22,7 +23,8 @@ class LogInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.log_in_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.log_in_fragment, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
